a588f91414ce587af6545be0b7358de6f6e5ac22
DeakinSimpson <deakinsimpson976@gmail.com>
2026-09-02 20:15:26 +1000

    add Order class
    this holds the basic information that each Order in the order book needs

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,52 @@
+#include <cfloat>
 #include <iostream>
+#include <sys/types.h>
+
+enum class OrderType
+{
+  MarketOrder,
+  LimitOrder,
+  // TODO: Implement Later
+  StopMarketOrder,
+  StopLimitOrder,
+  TrailingStopOrder,
+};
+
+enum class Side
+{
+  Bid,
+  Ask,
+};
+
+using OrderId = int;
+using Price = int;              
+using Quantity = unsigned int;  // cant have negative stock
+
+class Order
+{
+public:
+  Order(OrderId id, Side side, Price price,
+    Quantity quantity, OrderType orderType)
+      : id_{ id }
+      , side_{ side }
+      , price_{ price }
+      , quantity_{ quantity }
+      , orderType_{ orderType }
+  {  };
+
+  OrderId GetId() { return id_; }
+  Side GetSide() { return side_; }
+  Price GetPrice() { return price_; }
+  Quantity GetQuantity() { return quantity_; }
+  OrderType GetOrderType() { return orderType_; }
+
+private:
+  OrderId id_;
+  Side side_;
+  Price price_;
+  Quantity quantity_;
+  OrderType orderType_;
+};
 
 int main () {
   std::cout << "Hello Test!" << std::endl;
