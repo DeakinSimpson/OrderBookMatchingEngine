78733ee462b81b3d189fb349766009a97388e709
DeakinSimpson <deakinsimpson976@gmail.com>
2026-09-05 17:23:17 +1000

    add argv passing instead of hard coded file paths

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "orderbook.hpp"
 #include "ingest.hpp"
-int main () {
-  FileIterator fi { "data/ingest/xnas-itch-20260831.mbo.csv" };
+int main (int argc, char* argv[]) {
+  std::vector<std::string_view> args(argv + 1, argv + argc);
+  FileIterator fi { args.at(0).data() };
 
   OrderBook orderBook {};
 
